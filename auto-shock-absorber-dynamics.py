fd2859f697bc4b41c3463e544d3237c11e6fea47
oicnanev <voicnanev@proton.me>
2023-11-30 12:00:18 +0000

    plot and query_menu

--- auto-shock-absorber-dynamics.py
+++ auto-shock-absorber-dynamics.py
@@ -7,15 +7,18 @@ import math  # for math functions
 import numpy as np
 from matplotlib import pyplot as plt
 
-# GLOBAL VARIABLES -----------------------------------------------------------
-records = {}
-number_of_records = 0
-
 
 def main() -> None:
     """main function"""
+    if check_records_file_exists():
+        records = read_file()
+        number_of_records = len(records)
+    else:
+        records = {}
+        number_of_records = 0
     choice = main_menu()
     while choice != 5:
+        # calculate ------------------------------------------
         if choice == 1:
             calc_choices = calc_menu()
             b = calc_choices[0]
@@ -25,23 +28,26 @@ def main() -> None:
             t = calc_choices[4]
             if z == 1:
                 x, y = calc_critically_damped_system(k, m, t)
-                add_to_records(b, k, m, z, t, x, y)
-                plot(x, y)
             elif z > 1:
                 x, y = calc_over_damped_system(b, k, m, z, t)
-                add_to_records(b, k, m, z, t, x, y)
-                plot(x, y)
             else:
-                Y = calc_under_damped_system(k, m, z, t)
-                add_to_records(b, k, m, z, t, x, y)
-                plot(x, y)
+                x, y = calc_under_damped_system(k, m, z, t)
+            records[number_of_records] = (b, k, m, z, t, x, y)
+            number_of_records += 1
+            plot(records[number_of_records -1])
+        # query ----------------------------------------------
         elif choice == 2:
-            query_menu()
+            query_menu(records)
+        # load file ------------------------------------------
         elif choice == 3:
-            load_file()
+            records = load_file()
+            number_of_records = len(records)
+        # dump file ------------------------------------------
         elif choice == 4:
-            dump_file()
+            dump_file(records)
         choice = main_menu()
+    # dump file before exiting -------------------------------
+    dump_file(records)
     print("Bye")
     sys.exit(0)
 
@@ -57,11 +63,11 @@ def main_menu() -> int:
     print("4. Download file")
     print("5. Quit")
     print("=================================")
-    choice  = input("Enter your choice (1..5): ", end="")
+    choice  = input("Enter your choice (1..5): ")
 
     while choice not in ["1", "2", "3", "4", "5"]:
         print(f"{choice} in not a valid choice")
-        choice = input("Enter a valid choice (1..5): ", end="")
+        choice = input("Enter a valid choice (1..5): ")
 
     return int(choice)
 
@@ -130,26 +136,92 @@ def calc_menu() -> tuple:
     return (b, k, m, damping_coefficient, t)
 
 
-def query_menu() -> None:
+def query_menu(records: dict) -> None:
     """displays the query menu"""
     print("Query")
     print("=====")
-    # TODO: implement
+    print("1. Query by experiment number")
+    print("2. Query by b, K and M")
+    print("3. Return to main menu")
+    choice = input("Enter your choice (1..3): ")
+    while choice not in ["1", "2", "3"]:
+        print(f"{choice} in not a valid choice")
+        choice = input("Enter a valid choice (1..3): ")
+    if choice == 1:
+        print(f"There are {len(records)} experiments")
+        experiment_number = input("Enter the experiment number: ")
+        is_experiment_number_validated = False
+        while not is_experiment_number_validated:
+            for char in experiment_number:
+                if char not in [str(i) for i in range(len(records))]:
+                    print("Invalid input")
+                    experiment_number = input("Enter the experiment number: ")
+                else:
+                    is_experiment_number_validated = True
+                    experiment_number = int(experiment_number)
+        if experiment_number in records:
+            plot(records[experiment_number])
+            query_menu(records)
+        else:
+            print("Experiment number not found")
+    elif choice == 2:
+        allowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+        print("Enter the values of the following parameters:")
+        b = input("b (shock absorber): ")
+        is_b_validated = False
+        while not is_b_validated:
+            for char in b:
+                if char not in allowed:
+                    print("Invalid input")
+                    b = input("b (shock absorber): ")
+                else:
+                    is_b_validated = True
+                    b = float(b)
+        k = input("k (spring): ")
+        is_k_validated = False
+        while not is_k_validated:
+            for char in k:
+                if char not in allowed:
+                    print("Invalid input")
+                    k = input("k (spring): ")
+                else:
+                    is_k_validated = True
+                    k = float(k)
+        m = input("M (mass): ")
+        is_m_validated = False
+        while not is_m_validated:
+            for char in m:
+                if char not in allowed:
+                    print("Invalid input")
+                    m = input("M (mass): ")
+                else:
+                    is_m_validated = True
+                    m = float(m)
+        for key, value in records.items():
+            if value[0] == b and value[1] == k and value[2] == m:
+                print("Experiment number:", key)
+                plot(records[key])
+                query_menu(records)
+            else:
+                print("Experiment not found")
+                query_menu(records)
+    else:
+        main_menu()
 
 
-def load_file() -> None:
+def load_file() -> dict:
     """loads a file"""
-    print("Upload file")
+    print("Load records")
     print("===========")
-    print("file must be in the same directory as this program")
-    filename = input("Enter the filename: ")
-    read_file(filename)
+    return read_file()
 
 
-def dump_file() -> None:
+def dump_file(records: dict) -> None:
     """dumps a file"""
-    print("Download file")
+    print("Save records")
     print("=============")
+    write_file(records)
+
 
 # CALCULATIONS ----------------------------------------------------------------
 def calc_critically_damped_system(k: float, m: float, t: float) -> tuple:
@@ -200,47 +272,52 @@ def calc_under_damped_system(k: float, m: float, z: float, t: float) -> tuple:
 
 
 # PLOTTING --------------------------------------------------------------------
-def plot(x: int, y: float) -> None:
-    """plots the graph"""
-    scale_x = np.arange(0, x, .001)
-    scale_y = np.arange(0, y, .001)
-    plt.plot(scale_x, scale_y, y)
+def plot(record: tuple) -> None:
+    """plots the graph (b, k, m, z, t, x, y)"""
+    scale_x = np.arange(0, record[5], 1)
+    scale_y = record[6]
+    plt.plot(scale_x, scale_y, marker="o", linestyle="solid", color="blue")
+    plt.title("Displacement vs Time")
+    plt.suptitle(f"b = {record[0]}, K = {record[1]}, M = {record[2]}, z = {record[3]}, t = {record[4]}")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Displacement (m)")
+    plt.show()
+
 
 # I/O FUNCTIONS ---------------------------------------------------------------
-def read_file(filename: str) -> None:
-    """reads a file"""
-    global records
-
+def read_file() -> None:
+    """reads a file and returns the records"""
     try:
-        with open(filename, "rb") as file:
-            records = pickle.load(file)
-            print("file loaded")
+        with open("records", "rb") as file:
+            return pickle.load(file)
     except FileNotFoundError:
         print("File not found")
-        load_file()
+        sys.exit(1)
     except Exception:
         print("Unexpected error:", sys.exc_info()[0])
-        load_file()
+        sys.exit(2)
 
 
-def write_file() -> None:
+def write_file(records: dict) -> None:
     """writes a file"""
-    global records
-
     try:
         with open("records", "wb") as file:
             pickle.dump(records, file)
             print("file dumped")
     except Exception:
         print("Unexpected error:", sys.exc_info()[0])
-        dump_file()
+        sys.exit(3)
 
 
-def add_to_records(b: float, k: float, m: float, z: float, t: float, x: int, y: float) -> None:
-    """updates the records"""
-    global records
-    number_of_records += 1
-    records[number_of_records] = (b, k, m, z, t, x, y)
+def check_records_file_exists() -> bool:
+    """checks if the records file exists"""
+    try:
+        with open("records", "rb") as file:
+            file.close()
+    except FileNotFoundError:
+        with open("records", "wb") as file:
+            return False
+    return True
 
 
 # To start the program automatically
